8abec995c811fb667cbf1838caa67954fac99f8e
Evgenija26 <98258381+Evgenija26@users.noreply.github.com>
2023-02-07 16:26:06 +0200

    /

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,14 +1,16 @@
 import { useDispatch } from 'react-redux';
 import { logOut } from 'redux/auth/operations';
-// import { useAuth } from 'hooks/useAuth';
+import { selectUser } from 'redux/auth/selectors';
+import { useSelector } from 'react-redux';
 import css from './UserMenu.module.css';
 
 export const UserMenu = () => {
+  const user = useSelector(selectUser);
   const dispatch = useDispatch();
   // const { user } = useAuth;
   return (
     <div className={css.wrapper}>
-      <p className={css.username}>Welcome to Phonebook </p>
+      <p className={css.username}>Welcome to Phonebook {user.name} </p>
       <button type="button" onClick={() => dispatch(logOut())}>
         Logout
       </button>
